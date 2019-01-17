5b9f018d9714777fd684cd64064b0fe617f245ec
lyancsie <info@fordit.biz>
2019-01-17 20:29:21 +0100

    Custom http status code dropped in the case of invalid token

--- src/main/java/com/example/demo/Security/WebSecurity.java
+++ src/main/java/com/example/demo/Security/WebSecurity.java
@@ -45,7 +45,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         .addFilter(new JWTAuthenticationFilter(authenticationManager()))
         .addFilter(new JWTAuthorizationFilter(authenticationManager()))
         // this disables session creation on Spring Security
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(new Http401AuthenticationEntryPoint("No authorization"));
   }
   
   @Override
